d72bb9368c98a33849bcfd4ad24947802fa5177f
Mert Köklü <kklumert@gmail.com>
2023-05-07 12:09:07 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -1,218 +1,216 @@
+
+from firestore import FirestoreDB
+from update import Update
+from request_actor import GRequest
+import os
+
+
+import pytest
+
+"""
+Code Analysis
+
+Main functionalities:
+The Update class is responsible for updating the Firestore database with data fetched from the GitHub API. It initializes the necessary collections and documents in the database, and provides methods for fetching and writing data for each repository. It also has methods for generating overall statistics for all repositories in the ecosystem, and writing them to the database.
+
+Methods:
+- seed(): seeds the database with data for all repositories in the ecosystem
+- init_overall(): initializes the overall_data dictionary with default values for the overall statistics
+- write_overall(): generates and writes the overall statistics to the database
+- fetch_and_write(): fetches and writes data for a single repository to the database
+- overall_total_project_count(): generates and writes the total number of projects in the ecosystem to the database
+- overall_top_contributors(): generates and writes the top contributors for the ecosystem to the database
+- overall_recent_commits(): generates and writes the recent commits for the ecosystem to the database
+- overall_pull_request_count(): generates and writes the pull request count for the ecosystem to the database
+- overall_issue_count(): generates and writes the issue count for the ecosystem to the database
+- overall_issue_activity(): generates and writes the issue activity for the ecosystem to the database
+- overall_code_frequency(): generates and writes the code frequency for the ecosystem to the database
+- overall_commit_history(): generates and writes the commit history for the ecosystem to the database
+
+Fields:
+- make_request: an instance of the GRequest class for making requests to the GitHub API
+- data: a dictionary for storing data fetched from the GitHub API
+- collection_index: an integer for keeping track of the current collection index
+- fire_app: an instance of the FirestoreDB class for accessing the Firebase app
+- db: an instance of the FirestoreDB class for accessing the Firestore database
+- info_collection_ref: a reference to the collection for storing repository info documents
+- data_collection_ref: a reference to the collection for storing repository data documents
+- overall_collection_ref: a reference to the collection for storing overall statistics documents
+- all_repo_names: a list of dictionaries containing the names of all repositories in the ecosystem
+"""
+
+class TestUpdate:
     # Tests that the database is seeded correctly with valid data. 
     def test_seed(self):
-        # Happy path test
-        # Ensure that the database is seeded correctly with valid data
-        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
+        # setup
+        fire_ctx = FirestoreDB(admin_sdk_path='/Users/mert/Projects/eco-pulse/eco-pulse-frontend-private/admin_sdks', project_id='test-project')
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
+
+        # execute
         update.seed()
-        overall_data = update.overall_data
-        assert overall_data['total_project_count'] == 3
-        assert overall_data['total_star_count'] == 10
-        assert len(overall_data['top_contributors']) == 4
-        assert len(overall_data['recent_commits']) == 5
-        assert overall_data['issue_count']['series'][0]['data'][0]['value'] == 2
-        assert overall_data['issue_count']['series'][0]['data'][1]['value'] == 1
-        assert overall_data['pull_request_count']['series'][0]['data'][0]['value'] == 1
-        assert overall_data['pull_request_count']['series'][0]['data'][1]['value'] == 1
 
+        # assert
+        assert update.overall_data['total_project_count'] == len(update.all_repo_names)
+        assert update.overall_data['total_star_count'] >= 0
+        assert update.overall_data['top_contributors'] != {}
+        assert update.overall_data['commit_history']['xAxis']['data'] is not None
+        assert update.overall_data['_code_frequency'] != {}
+        assert update.overall_data['_issue_activity'] != {}
+        assert update.overall_data['recent_issues'] != []
+        assert update.overall_data['recent_commits'] != []
+        assert update.overall_data['issue_count']['series'][0]['data'][0]['value'] >= 0
+        assert update.overall_data['issue_count']['series'][0]['data'][1]['value'] >= 0
+        assert update.overall_data['pull_request_count']['series'][0]['data'][0]['value'] >= 0
+        assert update.overall_data['pull_request_count']['series'][0]['data'][1]['value'] >= 0
 
-
-    # Tests that data is fetched and written correctly for a repository with valid inputs.  
-    def test_fetch_and_write_valid(self):
-        # Happy path test
-        # Ensure that data is fetched and written correctly for a repository with valid inputs
+    # Tests that data is fetched and written correctly for a repository with valid input. 
+    def test_fetch_and_write_valid(self, mocker):
+        # setup
         fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
         repo = {'owner': 'test-owner', 'repo': 'test-repo'}
+        mocker.patch.object(GRequest, 'github_post_rest_request', return_value={'data': {'repository': {'name': 'test-repo'}}})
+        mocker.patch.object(GRequest, 'github_post_graphql_request', return_value={'data': {'repository': {'name': 'test-repo'}}})
+
+        # execute
         update.fetch_and_write(repo)
-        data_doc_ref = update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}")
-        data = data_doc_ref.get().to_dict()
 
-        # Assert that all keys are present in the fetched data
-        expected_keys = ['commit_history', 'code_frequency', 'recent_issues', 'issue_activity',
-                         'issue_count', 'pull_request_count', 'pull_request_activity',
-                         'star_activity', 'top_contributors', 'recent_commits']
-        assert set(data.keys()) == set(expected_keys)
+        # assert
+        assert update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}").get().exists
 
-        # Assert that all values are not None
-        for key in expected_keys:
-            assert data[key] is not None, f"{key} should not be None"
-
-
-    # Tests that API errors are handled correctly during data fetching. 
-    def test_api_error_handling(self, monkeypatch):
-        # Edge case test
-        # Ensure that API errors are handled correctly during data fetching
-        def mock_post_graphql_request(_query, variables=None):
-            return None
-
-        monkeypatch.setattr(GRequest, 'github_post_graphql_request', mock_post_graphql_request)
-
+    # Tests that an error is raised when trying to fetch and write data for a non-existent repository. 
+    def test_fetch_and_write_nonexistent(self, mocker):
+        # setup
         fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
-        repo = {'owner': 'test-owner', 'repo': 'test-repo'}
-        update.fetch_and_write(repo)
-        data_doc_ref = update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}")
-        data = data_doc_ref.get().to_dict()
-        assert data is None
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
+        repo = {'owner': 'nonexistent-owner', 'repo': 'nonexistent-repo'}
+        mocker.patch.object(GRequest, 'github_post_rest_request', return_value=None)
+        mocker.patch.object(GRequest, 'github_post_graphql_request', return_value=None)
 
-
-
-    # Tests that unexpected responses from the API are handled correctly. 
-    def test_unexpected_response_handling(self, monkeypatch):
-        # Edge case test
-        # Ensure that unexpected responses from the API are handled correctly
-        def mock_post_graphql_request(_query, variables=None):
-            return {'data': {'repository': {'name': 'test-repo', 'owner': {'login': 'test-owner'}}}}
-
-        monkeypatch.setattr(GRequest, 'github_post_graphql_request', mock_post_graphql_request)
-
-        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
-        repo = {'owner': 'test-owner', 'repo': 'test-repo'}
-        update.fetch_and_write(repo)
-        data_doc_ref = update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}")
-        data = data_doc_ref.get().to_dict()
-        assert data is None
-
-
-    # Tests that unexpected errors during database operations are handled correctly. 
-    def test_unexpected_error_handling(self, monkeypatch):
-        # Edge case test
-        # Ensure that unexpected errors during database operations are handled correctly
-        def mock_commit_history(owner, repo):
-            raise Exception('Unexpected error')
-
-        monkeypatch.setattr(Update, 'commit_history', mock_commit_history)
-
-        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
-        repo = {'owner': 'test-owner', 'repo': 'test-repo'}
-        update.fetch_and_write(repo)
-        data_doc_ref = update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}")
-        data = data_doc_ref.get().to_dict()
-        assert data is None
-
-
-
-    # Tests that data fetching and writing fails gracefully for a repository with invalid inputs.  
-    def test_fetch_and_write_invalid(self):
-        # Edge case test
-        # Ensure that data fetching and writing fails gracefully for a repository with invalid inputs
-        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
-        update = Update(fire_ctx, 'test-ecosystem')
-        repo = {'owner': 'invalid-owner', 'repo': 'invalid-repo'}
-        
-        # Wrap the fetch_and_write call in a try-except block to handle exceptions
-        try:
+        # execute and assert
+        with pytest.raises(Exception):
             update.fetch_and_write(repo)
-        except Exception as e:
-            # Assert that the exception message is as expected
-            assert str(e) == f"Failed to fetch and write data for repository {repo['owner']}/{repo['repo']}"
 
-        # Check if the data was not written to the database
-        data_doc_ref = update.data_collection_ref.document(f"{repo['owner']}-{repo['repo']}")
-        data_doc = data_doc_ref.get()
-        
-        # Assert that the document does not exist in the database
-        assert not data_doc.exists
+    # Tests that an error is raised when trying to fetch and write data for a repository with invalid input. 
+    def test_fetch_and_write_invalid(self, mocker):
+        # setup
+        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
+        repo = {'owner': '', 'repo': ''}
+        mocker.patch.object(GRequest, 'github_post_rest_request', return_value=None)
+        mocker.patch.object(GRequest, 'github_post_graphql_request', return_value=None)
 
+        # execute and assert
+        with pytest.raises(Exception):
+            update.fetch_and_write(repo)
 
-        # Tests that overall data is written correctly to the database.  
+    # Tests that overall data is written correctly to the database. 
     def test_write_overall(self, mocker):
-        # Happy path test
-        mocker.patch.object(Update, 'overall_commit_history')
-        mocker.patch.object(Update, 'overall_code_frequency')
-        mocker.patch.object(Update, 'overall_issue_activity')
-        mocker.patch.object(Update, 'overall_issue_count')
-        mocker.patch.object(Update, 'overall_pull_request_count')
-        mocker.patch.object(Update, 'overall_recent_commits')
-        mocker.patch.object(Update, 'overall_top_contributors')
-        mocker.patch.object(Update, 'overall_total_project_count')
-        mocker.patch.object(Update, 'overall_star_count')
+        # setup
+        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
+        mocker.patch.object(update, 'overall_commit_history')
+        mocker.patch.object(update, 'overall_code_frequency')
+        mocker.patch.object(update, 'overall_issue_activity')
+        mocker.patch.object(update, 'overall_issue_count')
+        mocker.patch.object(update, 'overall_pull_request_count')
+        mocker.patch.object(update, 'overall_recent_commits')
+        mocker.patch.object(update, 'overall_top_contributors')
+        mocker.patch.object(update, 'overall_total_project_count')
+        mocker.patch.object(update, 'overall_star_count')
 
-        update = Update(None, "test_ecosystem")
-        update.overall_data = {
-            "commit_history": {
-                "xAxis": {"data": None},
-                "yAxis": {},
-                "series": [
-                    {"data": [], "type": "line"}
-                ],
-            },
-            "_code_frequency": {},
-            "_issue_activity": {},
-            "recent_issues": [],
-            "recent_commits": [],
-            "issue_count": {
-                "series": [
-                    {
-                        "data": [
-                            {"value": 0, "name": 'Open'},
-                            {"value": 0, "name": 'Closed'},
-                        ]
-                    }
-                ]
-            },
-            "pull_request_count": {
-                "series": [
-                    {
-                        "data": [
-                            {"value": 0, "name": 'Open'},
-                            {"value": 0, "name": 'Closed'},
-                        ]
-                    }
-                ]
-            },
-            "top_contributors": {},
-            "total_project_count": 0,
-            "total_star_count": 0
-        }
-
+        # execute
         update.write_overall()
 
-        update.overall_commit_history.assert_called_once()
-        update.overall_code_frequency.assert_called_once()
-        update.overall_issue_activity.assert_called_once()
-        update.overall_issue_count.assert_called_once()
-        update.overall_pull_request_count.assert_called_once()
-        update.overall_recent_commits.assert_called_once()
-        update.overall_top_contributors.assert_called_once()
-        update.overall_total_project_count.assert_called_once()
-        update.overall_star_count.assert_called_once()
+        # assert
+        assert update.overall_commit_history.called
+        assert update.overall_code_frequency.called
+        assert update.overall_issue_activity.called
+        assert update.overall_issue_count.called
+        assert update.overall_pull_request_count.called
+        assert update.overall_recent_commits.called
+        assert update.overall_top_contributors.called
+        assert update.overall_total_project_count.called
+        assert update.overall_star_count.called
 
+    # Tests that data is retrieved correctly from the database. 
+    def test_get_data(self):
+        # setup
+        fire_ctx = FirestoreDB(admin_sdk_path='path/to/credentials.json', project_id='test-project')
+        ecosystem = 'test-ecosystem'
+        update = Update(fire_ctx, ecosystem)
 
-
-            # Tests that top contributors are handled correctly when there are no contributors.  
-    def test_top_contributors_empty(self, mocker):
-        # Edge case test
-        mocker.patch.object(Update, 'top_contributors', return_value={})
-
-        update = Update(None, "test_ecosystem")
-        update.overall_data = {
-            "top_contributors": {}
-        }
-
-        update.overall_top_contributors()
-
-        update.overall_collection_ref.document.assert_called_with(u'_overall')
-        update.overall_collection_ref.document().set.assert_called_with({
-            u'top_contributors': None,
-        }, merge=True)
-
-
-            # Tests that data is retrieved correctly from the database.  
-    def test_get_data(self, mocker):
-        # General behavior test
-        mocker.patch.object(FirestoreDB, 'get_db')
-        mocker.patch.object(FirestoreDB, 'get_app')
-        mocker.patch.object(FirestoreDB, 'collection')
-        mocker.patch.object(FirestoreDB, 'document')
-        mocker.patch.object(GRequest, 'github_post_rest_request', return_value=[{"name": "test_repo"}])
-
-        update = Update(FirestoreDB(None, "test_project"), "test_ecosystem")
-        update.all_repo_names = [{"owner": "test_owner", "repo": "test_repo"}]
-
+        # execute
         data = update.get_data
 
-        assert data == [{"name": "test_repo"}]
+        # assert
+        assert data is not None
+
+    def test_code_frequency():
+        owner = "paritytech"
+        repo = "polkadot"
+        option = self.code_frequency(owner, repo)
+
+        # Check that the function returns a valid option
+        assert option is not None
+
+        # Check that the option has the expected keys
+        assert set(option.keys()) == {"xAxis", "yAxis", "series"}
+
+        # Check that the xAxis is a dictionary with the expected keys
+        assert set(option["xAxis"].keys()) == {"data"}
+
+        # Check that the yAxis is a dictionary
+        assert isinstance(option["yAxis"], dict)
+
+        # Check that the series is a list with two elements
+        assert isinstance(option["series"], list)
+        assert len(option["series"]) == 2
+
+        # Check that the series elements are dictionaries with the expected keys
+        for series_element in option["series"]:
+            assert set(series_element.keys()) == {"data", "type", "stack"}
+
+            # Check that the data for the series element is a list with the expected length
+            data = series_element["data"]
+            assert isinstance(data, list)
+            assert len(data) == 52
+
+        # Check that the overall_data has been updated correctly
+        assert self.overall_data["_code_frequency"] != {}
+
+
+    def test_commit_history():
+        owner = "paritytech"
+        repo = "polkadot"
+        option = self.commit_history(owner, repo)
+
+        # Check that the function returns a valid option
+        assert option is not None
+
+        # Check that the option has the expected keys
+        assert set(option.keys()) == {"xAxis", "yAxis", "series"}
+
+        # Check that the xAxis is a dictionary with the expected keys
+        assert set(option["xAxis"].keys()) == {"data"}
+
+        # Check that the yAxis is a dictionary
+        assert isinstance(option["yAxis"], dict)
+
+        # Check that the series is a list with one element
+        assert isinstance(option["series"], list)
+        assert len(option["series"]) == 1
+
+        # Check that the series element is a dictionary with the expected keys
+        series_element = option["series"][0]
+        assert set(series_element.keys()) == {"data", "type"}
+
+        # Check that the data for the series element is a list with the expected length
+        data = series_element["data"]
+        assert isinstance(data, list)
+        assert len(data) == 52
