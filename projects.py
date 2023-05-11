781f235468de4a200d16865d1b8a76fe719a2bee
Mert Köklü <kklumert@gmail.com>
2023-05-11 18:29:08 +0300

    fixed

--- projects.py
+++ projects.py
@@ -1,5 +1,5 @@
 projects = [
-# ("polka", "polkapulse", "polkapulse-c9c65"),
+("polka", "polkapulse", "polkapulse-c9c65"),
 # ("lens", "lenspulse", "lenspulse-20592"),
 # ("flow", "flowpulse", "flowpulse-11122"),
 
